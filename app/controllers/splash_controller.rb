03d4486d8ca392f7c2a333db78e8bc1caa6d2f9f
Christy Karpinski <christykarpinski@Christy23.local>
2023-08-03 09:53:42 -0500

    corrected rubocop offenses

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -2,14 +2,15 @@
 
 class SplashController < ProprietorController
   def index
-      @accounts = Account.where('is_public = ?', true).order(cname: :asc)
-      @images = []
-      @alt_text = []
-      @accounts.map do |account|
-        Apartment::Tenant.switch(account.tenant) do
-          @images << Site.instance&.directory_image&.url(:medium) || "https://via.placeholder.com/400?text=#{account.cname}"
-          @alt_text << Site.instance || account.cname
-        end
+    @accounts = Account.where('is_public = ?', true).order(cname: :asc)
+    @images = []
+    @alt_text = []
+    @accounts.map do |account|
+      Apartment::Tenant.switch(account.tenant) do
+        @images << Site.instance&.directory_image&.url(:medium) ||
+          "https://via.placeholder.com/400?text=#{account.cname}"
+        @alt_text << Site.instance || account.cname
       end
     end
+  end
 end
